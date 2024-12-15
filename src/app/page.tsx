24a424ab04cb109f100f2142995059a8ff9441cc
xhamyo <120670441+xhamyo@users.noreply.github.com>
2024-12-15 17:02:49 +0800

    Add Theme Feature

---
+++ src/app/page.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
+export default function Home() {
+
+  const router = useRouter();
+
+  return (
+    <main>
+      <div className="w-full px-8 py-12 md:py-20 flex flex-col items-center">
+        <h1 className="text-center text-4xl md:text-6xl max-w-xl font-semibold">
+          Hamyo
+        </h1>
+        <p className="text-center max-w-xl my-6">
+          Portfolio Page.
+        </p>
+        <button
+          data-testid='test-button'
+          onClick={() => {router.push("/test");}}
+        >
+          Test
+        </button>
+      </div>
+    </main>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/components/**/*.{tsx,ts,jsx,js}",
+    "./src/app/**/*.{tsx,ts,jsx,js}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  darkMode: "class",
+}
