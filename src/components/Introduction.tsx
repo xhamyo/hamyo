b6e469d622d72921ed5cd4154b15b8b022db4b23
xhamyo <120670441+xhamyo@users.noreply.github.com>
2024-12-25 04:34:34 +0800

    Test Font Size

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -20,7 +20,7 @@ export default function Introduction() {
     <div className="border-4 rounded-xl border-gray-200 bg-gray-100 dark:border-gray-900 dark:bg-gray-800 w-[90vw] md:w-[48rem]">
       <div className="px-2 py-2 md:px-4 md:py-4 flex flex-row items-center space-x-2 md:space-x-4">
         <Image alt="logo" src="/images/hanyu.png" width={90} height={90} className="rounded-md w-[15vw] w-[15vw]"/>
-        <div className="text-4xl md:text-6xl font-semibold tracking-tight">
+        <div className="text-2xl md:text-6xl font-semibold tracking-tight">
           <h1>hello! I am Han Yu,</h1>
           <div className="relative flex flex-row overflow-hidden">
             <AnimatePresence mode="popLayout">
