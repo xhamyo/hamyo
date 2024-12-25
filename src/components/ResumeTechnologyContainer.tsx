6078ffef1bde371e128de65146b9316edb2230ff
xhamyo <120670441+xhamyo@users.noreply.github.com>
2024-12-25 22:34:00 +0800

    Test Font Size II

--- src/components/ResumeTechnologyContainer.tsx
+++ src/components/ResumeTechnologyContainer.tsx
@@ -11,7 +11,7 @@ interface ResumeTechnologyContainerProps {
 }
 const ResumeTechnologyContainer: React.FC<ResumeTechnologyContainerProps> = ({ icon1, technology1, icon2, technology2, icon3, technology3 }) => {
   return (
-    <div className="flex flex-row items-center justify-center space-x-4 md:space-x-6 font-bold text-md md:text-2xl">
+    <div className="flex flex-row items-center justify-center space-x-4 md:space-x-6 font-bold text-sm md:text-2xl">
       <div className="flex flex-col items-center justify-center rounded-lg space-y-4 w-[25vw] h-[20vw] md:w-[14rem] md:h-[11rem] hover:scale-[1.05] shadow-lg dark:shadow-gray-500 dark:shadow-md dark:bg-gray-900 transition-all duration-200 ease-in-out">
         <div className="text-3xl md:text-5xl">
           {icon1}
