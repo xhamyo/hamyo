fcbd9f7d21acf48ae637d25b61b159e234575257
xhamyo <120670441+xhamyo@users.noreply.github.com>
2024-12-26 01:50:49 +0800

    Update Introduction
    Updated colours.

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -20,8 +20,8 @@ export default function Introduction() {
     <div className="rounded-lg bg-zinc-100 dark:bg-gray-950 w-[90vw] md:w-[48rem]">
       <div className="px-3 py-3 md:px-4 md:py-4 flex flex-row items-center space-x-2 md:space-x-4">
         <Image alt="logo" src="/images/hanyu.png" width={90} height={90} className="rounded-md w-[15vw]"/>
-        <div className="text-2xl md:text-6xl font-semibold tracking-tight">
-          <h1>hello! I am Han Yu,</h1>
+        <div className="text-2xl md:text-6xl tracking-tight">
+          <h1>hello! I am <span className="text-orange-500 font-bold">HAN YU</span>,</h1>
           <div className="relative flex flex-row overflow-hidden">
             <AnimatePresence mode="popLayout">
               <motion.div
@@ -30,7 +30,7 @@ export default function Introduction() {
                 animate={{ y: "0%", opacity: 1 }}
                 exit={{ y: "-100%", opacity: 1 }}
                 transition={{ duration: 0.5 }}
-                className="text-indigo-500 font-bold w-[65vw] md:w-[38rem]"
+                className="text-indigo-500 font-semibold w-[65vw] md:w-[38rem]"
               >
                 {words[index]}
               </motion.div>
