5c61fdf8b0b32916afff52f1237c3fb10c951df4
xhamyo <120670441+xhamyo@users.noreply.github.com>
2024-12-26 00:36:46 +0800

    Test Font Size II

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -17,20 +17,20 @@ export default function Contact() {
           <h1 className="md:text-xl text-gray-600 dark:text-gray-300">
               Please contact me for any enquiries!
           </h1>
-          <div className="flex flex-row items-center px-3 py-3 md:px-4 md:py-4 space-x-2 rounded-xl shadow-xl w-[80vw] h-[45vw] md:w-[30rem] md:h-[17rem] hover:scale-[1.05] shadow-xl shadow-gray-600 dark:shadow-gray-300 dark:shadow-md bg-gray-300 dark:bg-gray-600 transition-all duration-200 ease-in-out">
+          <div className="flex flex-row items-center px-3 py-3 md:px-4 md:py-4 space-x-2 rounded-xl shadow-xl w-[22rem] h-[12.5rem] md:w-[30rem] md:h-[17rem] hover:scale-[1.05] shadow-xl shadow-gray-600 dark:shadow-gray-300 dark:shadow-md bg-gray-300 dark:bg-gray-600 transition-all duration-200 ease-in-out">
             <Image alt="logo" src="/images/hanyu.png" width={90} height={90} className="w-1/3 border-8 border-zinc-900 rounded-full shadow-2xl hover:scale-[1.05] transition-all duration-200 ease-in-out"/>
             <div className="flex flex-col">
               <h1 className="text-indigo-600 dark:text-indigo-400 font-bold text-2xl">Han Yu</h1>
               <h2 className="font-bold text-md md:text-xl">Software Engineer</h2>
-              <div className="flex flex-row space-x-3 text-sm md:space-x-6">
-                <div className="flex flex-col font-bold text-sm md:text-lg">
+              <div className="flex flex-row space-x-2 text-sm md:space-x-6">
+                <div className="flex flex-col font-bold text-xs md:text-lg">
                   <h3>Phone</h3>
                   <h3>Email</h3>
                   <h3>LinkedIn</h3>
                   <h3>Instagram</h3>
                   <h3>Telegram</h3>
                 </div>
-                <div className="flex flex-col font-light md:text-lg">
+                <div className="flex flex-col font-light text-xs md:text-lg">
                   <a href="tel:+6587164969" className="hover:underline hover:text-gray-700 hover:dark:text-gray-400">
                     (+65) 8716 4969<br />
                   </a>
