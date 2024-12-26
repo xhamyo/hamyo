030a5429e238117047309332cd638b851f40430a
xhamyo <120670441+xhamyo@users.noreply.github.com>
2024-12-27 01:07:23 +0800

    Debugging

--- src/components/sections/ProjectContainer.tsx
+++ src/components/sections/ProjectContainer.tsx
@@ -21,10 +21,10 @@ const ProjectContainer: React.FC<ProjectContainerProps> = ({ title, images, imag
   }, []);
   
   return (
-    <div className="flex flex-col md:flex-row p-3 md:p-4 space-y-2 md:space-x-4 rounded-lg hover:scale-[1.01] shadow-xl dark:shadow-gray-500 dark:shadow-md transition-all duration-200 ease-in-out dark:bg-gradient-to-r dark:from-gray-800 dark:via-gray-900 dark:to-gray-800">
+    // <div className="flex flex-col md:flex-row p-3 md:p-4 space-y-2 md:space-x-4 rounded-lg hover:scale-[1.01] shadow-xl dark:shadow-gray-500 dark:shadow-md transition-all duration-200 ease-in-out dark:bg-gradient-to-r dark:from-gray-800 dark:via-gray-900 dark:to-gray-800">
       
-      {/* Image and Description */}
-      <div className="flex flex-col md:w-1/2 items-center">
+    //   {/* Image and Description */}
+       <div className="flex flex-col md:w-1/2 items-center">
         <div className="relative overflow-hidden hover:scale-[1.1] transition-all duration-200 ease-in-out">
           <AnimatePresence mode="popLayout">
             <motion.div
@@ -39,32 +39,33 @@ const ProjectContainer: React.FC<ProjectContainerProps> = ({ title, images, imag
             </motion.div>
           </AnimatePresence>
         </div>
-        <div className="relative overflow-hidden hover:scale-[1.01] transition-all duration-200 ease-in-out">
-          <AnimatePresence mode="popLayout">
-            <motion.div
-              key={imageIndex}
-              initial={{ x: "100%", opacity: 1 }}
-              animate={{ x: "0%", opacity: 1 }}
-              exit={{ x: "-100%", opacity: 1 }}
-              transition={{ duration: 1.0 }}
-              className="border-4 border-red-500 w-[80vw] md:w-[22rem] text-center font-semibold italic text-lg md:text-xl"
-            >
-              {imageDescriptions[imageIndex]}
-            </motion.div>
-          </AnimatePresence>
-        </div>
-      </div>
-
-      {/* Title and Content */}
-      <div className="flex flex-col md:w-1/2 space-y-2">
-        <h1 className="font-bold text-lg md:text-xl">
-          {title}
-        </h1>
-        <h1 className="font-light text-gray-600 dark:text-gray-300">
-          {content}
-        </h1>
-      </div>
     </div>
+    //     <div className="relative overflow-hidden hover:scale-[1.01] transition-all duration-200 ease-in-out">
+    //       <AnimatePresence mode="popLayout">
+    //         <motion.div
+    //           key={imageIndex}
+    //           initial={{ x: "100%", opacity: 1 }}
+    //           animate={{ x: "0%", opacity: 1 }}
+    //           exit={{ x: "-100%", opacity: 1 }}
+    //           transition={{ duration: 1.0 }}
+    //           className="border-4 border-red-500 w-[80vw] md:w-[22rem] text-center font-semibold italic text-lg md:text-xl"
+    //         >
+    //           {imageDescriptions[imageIndex]}
+    //         </motion.div>
+    //       </AnimatePresence>
+    //     </div>
+    //   </div>
+
+    //   {/* Title and Content */}
+    //   <div className="flex flex-col md:w-1/2 space-y-2">
+    //     <h1 className="font-bold text-lg md:text-xl">
+    //       {title}
+    //     </h1>
+    //     <h1 className="font-light text-gray-600 dark:text-gray-300">
+    //       {content}
+    //     </h1>
+    //   </div>
+    // </div>
   );
 }
 
