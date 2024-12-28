41226a93bd86ad580602372986b6c2d2e3d8d0b8
xhamyo <120670441+xhamyo@users.noreply.github.com>
2024-12-28 12:42:54 +0800

    Update Padding

--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -5,7 +5,7 @@ export default function About() {
   
   return (
     <div className="rounded-lg bg-zinc-100 dark:bg-zinc-950 w-[90vw] md:w-[50vw] shadow-lg dark:shadow-gray-800">
-      <div className="px-3 py-3 md:px-4 md:py-4 flex flex-col justify-center space-y-2 md:space-y-4">
+      <div className="p-4 md:p-8 flex flex-col justify-center space-y-2 md:space-y-4">
         <div className="flex flex-row space-x-2 font-semibold tracking-light">
           <IdcardOutlined className="text-3xl md:text-4xl"/>
           <h1 className="text-2xl md:text-3xl">
