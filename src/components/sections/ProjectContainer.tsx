826cd6acbb486f723fc2a5bd5ee49abd413b03ab
xhamyo <120670441+xhamyo@users.noreply.github.com>
2024-12-28 10:41:57 +0800

    Update Project Container
    Added date and type.

--- src/components/sections/ProjectContainer.tsx
+++ src/components/sections/ProjectContainer.tsx
@@ -5,11 +5,13 @@ import { motion, AnimatePresence } from "framer-motion";
 
 interface ProjectContainerProps {
   title: String;
+  date: String;
+  type: String;
   images: ReactNode[];
   imageDescriptions: String[];
   content: String;
 }
-const ProjectContainer: React.FC<ProjectContainerProps> = ({ title, images, imageDescriptions, content }) => {
+const ProjectContainer: React.FC<ProjectContainerProps> = ({ title, date, type, images, imageDescriptions, content }) => {
   
   const [ imageIndex, setImageIndex ] = useState(0);
 
@@ -21,7 +23,7 @@ const ProjectContainer: React.FC<ProjectContainerProps> = ({ title, images, imag
   }, []);
   
   return (
-    <div className="flex flex-col md:flex-row p-3 md:p-4 space-y-2 md:space-x-4 rounded-lg hover:scale-[1.01] shadow-xl dark:shadow-gray-500 dark:shadow-md transition-all duration-200 ease-in-out dark:bg-gradient-to-r dark:from-gray-800 dark:via-gray-900 dark:to-gray-800">
+    <div className="flex flex-col md:flex-row p-3 md:p-4 space-y-2 md:space-x-4 rounded-lg hover:scale-[1.01] shadow-lg dark:shadow-gray-500 dark:shadow-md transition-all duration-200 ease-in-out dark:bg-gradient-to-r dark:from-gray-800 dark:via-gray-900 dark:to-gray-800">
       
       {/* Image and Description */}
        <div className="flex flex-col md:w-1/2 items-center">
@@ -56,11 +58,19 @@ const ProjectContainer: React.FC<ProjectContainerProps> = ({ title, images, imag
       </div>
 
       {/* Title and Content */}
-      <div className="flex flex-col md:w-1/2 space-y-2">
-        <h1 className="font-bold text-lg md:text-xl">
-          {title}
+      <div className="flex flex-col md:w-1/2">
+        <div className="flex flex-row justify-between font-bold text-lg md:text-xl">
+          <h1>
+            {title}
+          </h1>
+          <h1 className="text-indigo-500">
+            {date}
+          </h1>
+        </div>
+        <h1 className="text-justify font-bold tracking-wide text-gray-600 dark:text-gray-300">
+          {type}
         </h1>
-        <h1 className="font-light text-gray-600 dark:text-gray-300">
+        <h1 className="text-justify font-light text-gray-600 dark:text-gray-300">
           {content}
         </h1>
       </div>
--- src/components/sections/Projects.tsx
+++ src/components/sections/Projects.tsx
@@ -8,7 +8,7 @@ export default function Projects() {
   
   return (
     <div className="rounded-lg bg-zinc-100 dark:bg-zinc-950 w-[90vw] md:w-[48rem] shadow-lg dark:shadow-gray-800">
-      <div className="px-3 py-3 md:px-4 md:py-4 flex flex-col justify-center space-y-2 md:space-y-4">
+      <div className="px-3 py-3 md:px-4 md:py-4 flex flex-col justify-center space-y-3 md:space-y-6">
         <div className="flex flex-row space-x-2 font-semibold tracking-light">
           <FaHammer className="text-3xl md:text-4xl"/>
           <h1 className="text-2xl md:text-3xl">
@@ -17,6 +17,8 @@ export default function Projects() {
         </div>
         <ProjectContainer
           title="Project Title"
+          date="Project Date"
+          type="Project Type"
           images={[
             <Image alt="logo" src="/images/placeholder.png" width={50} height={50} className="w-full"/>,
             <Image alt="logo" src="/images/placeholder.png" width={50} height={50} className="w-full"/>,
@@ -24,6 +26,29 @@ export default function Projects() {
           imageDescriptions={["Project Description", "Project Long Description", "Project Longest Long Description 3"]}
           content="Project content. Project content. Project content. Project content. Project content. Project content. Project content. Project content. Project content. Project content. Project content. Project content. Project content. Project content. Project content. Project content. Project content. Project content. Project content. Project content."
         />
+        <ProjectContainer
+          title="Project Title"
+          date="Project Date"
+          type="Project Type"
+          images={[
+            <Image alt="logo" src="/images/placeholder.png" width={50} height={50} className="w-full"/>,
+            <Image alt="logo" src="/images/placeholder.png" width={50} height={50} className="w-full"/>,
+            <Image alt="logo" src="/images/placeholder.png" width={50} height={50} className="w-full"/>]}
+          imageDescriptions={["Project Description", "Project Long Description", "Project Longest Long Description 3"]}
+          content="Project content. Project content. Project content. Project content. Project content. Project content. Project content. Project content. Project content. Project content. Project content. Project content. Project content. Project content. Project content. Project content. Project content. Project content. Project content. Project content."
+        />
+        <ProjectContainer
+          title="Project Title"
+          date="Project Date"
+          type="Project Type"
+          images={[
+            <Image alt="logo" src="/images/placeholder.png" width={50} height={50} className="w-full"/>,
+            <Image alt="logo" src="/images/placeholder.png" width={50} height={50} className="w-full"/>,
+            <Image alt="logo" src="/images/placeholder.png" width={50} height={50} className="w-full"/>]}
+          imageDescriptions={["Project Description", "Project Long Description", "Project Longest Long Description 3"]}
+          content="Project content. Project content. Project content. Project content. Project content. Project content. Project content. Project content. Project content. Project content. Project content. Project content. Project content. Project content. Project content. Project content. Project content. Project content. Project content. Project content."
+        />
+        <div className="h-1"></div>
       </div>
     </div>
   );
